1aad76dca38323acc6e460847b82d82d58a07e16
HippywithanAK <rjrferens@gmail.com>
2019-12-13 10:49:35 +1300

    psedocode for calculator.js

---
+++ calculator.js
@@ -0,0 +1,44 @@
+// Array to hold entries
+
+//variable to hold current total
+
+//variable to hold numbers or '.' pressed as string
+
+//variable to store latest button press 
+
+//check if last button pressed was a number or '.'
+//if so, add to our, number holding, string variable.
+//set screen to display this string, up to 10 characters, starting with the first button pressed.
+
+//if AC pressed, clear entries array & holding string, set total to 0 and set screen to display empty string
+
+//if CE pressed, clear string and screen
+
+//if x pressed, push string into entries array
+//push * into entries array
+// clear string
+
+//if ÷ pressed, push string to entries array
+//push / to entries array
+//clear string
+
+//if = pressed push string to entries array
+//variable to hold first entry in entries array
+//loop to set varible to hold the next number in entries array
+//and variable to hold current symbol
+//check what the current symbol is
+// if + first entry += next number
+// if - first entry -= next number
+// if * first entry *= next number
+// if / first entry /= next number
+// exit loop
+
+// make sure screen can display negative numbers
+
+// set screen value and clear entries aray and string
+
+// else push string to entries array
+// last button press to entries array
+// clear string
+
+
